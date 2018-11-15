a60026c615ae4de20612dea125b21dd398a7cc17
TinotendaMatsika <TinotendaMatsika@s1020n67h197.wst2.mun.ca>
2018-11-15 11:21:39 -0330

    edited LoginUI

--- activityTracker/src/activityTracker/loginUI.java
+++ activityTracker/src/activityTracker/loginUI.java
@@ -1,4 +1,39 @@
+package activityTracker;
 
-public class loginUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.*;
 
-}
+public class loginUI  {
+	
+	public static void main(String[]args) {
+		JFrame frame = new JFrame("Activity Tracker");
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(800,600));	
+		
+		JPanel p = new JPanel(new BorderLayout());
+		p.setBackground(Color.CYAN);
+		
+		JPanel button = new JPanel();
+        JButton CreateAccount = new JButton("Create Account");
+        CreateAccount.setFont(new Font("Serif",Font.PLAIN,27));
+        button.add(CreateAccount);
+        JButton Login = new JButton("LogIn");
+        Login.setFont(new Font("Serif",Font.PLAIN,27));
+        button.add(Login);
+        
+        
+        p.add(button, BorderLayout.CENTER);
+		
+		frame.add(p);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+  
+        
+	}
+
