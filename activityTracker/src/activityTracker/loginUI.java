c4bf08cc7d84894f57aa58b2b41d62e44a23dd51
macp1 <macp1@10.20.220.173>
2018-11-15 15:51:05 -0330

    added textfields and labels

--- activityTracker/src/activityTracker/loginUI.java
+++ activityTracker/src/activityTracker/loginUI.java
@@ -1,10 +1,13 @@
 package activityTracker;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.*;
+
 //test
 
 public class loginUI  {
@@ -26,8 +29,25 @@ public class loginUI  {
         Login.setFont(new Font("Serif",Font.PLAIN,27));
         button.add(Login);
         
+        JPanel textFields = new JPanel();
+        textFields.setLayout(new BoxLayout(textFields, BoxLayout.Y_AXIS));
+        JTextField usernameTextField = new JTextField(30);
+        JLabel usernameLabel = new JLabel("Username: ");
+        usernameLabel.setFont(new Font("Serif",Font.PLAIN,27));
+        usernameTextField.setFont(new Font("Serif",Font.PLAIN,27));
+        JTextField passwordTextField = new JTextField(30);
+        JLabel passwordLabel = new JLabel("Password: ");
+        passwordLabel.setFont(new Font("Serif",Font.PLAIN,27));
+        passwordTextField.setFont(new Font("Serif",Font.PLAIN,27));
+        textFields.add(usernameLabel);
+        textFields.add(usernameTextField, BorderLayout.NORTH);
+        textFields.add(passwordLabel);
+        textFields.add(passwordTextField, BorderLayout.SOUTH);
         
-        p.add(button, BorderLayout.CENTER);
+        
+        
+        p.add(button, BorderLayout.NORTH);
+        p.add(textFields, BorderLayout.CENTER);
 		
 		frame.add(p);
 		frame.pack();
